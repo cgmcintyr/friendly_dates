7c0bb6e149466e8124c05e56af694ad1be37ebfa
oploid <oploid7@gmail.com>
2015-04-05 17:22:46 +0100

    Made end date an optional argument. When the end date is supplied, friendlify returns the interval between the two dates as a human friendly string. When the end date is not supplied, friendlify returns just the starting date as a human friendly string. I also improved the type checking of friendlify's inputs.

--- friendly_dates.py
+++ friendly_dates.py
@@ -8,16 +8,47 @@ def string_to_datetime(date_string):
     date = datetime.datetime.strptime(date_string, "%d-%m-%Y")
     return(date)
 
-def check_interval(start_date, end_date):
+def check_valid_input(start_date, end_date):
     """
-    Checks two datetime objects against eachother to make sure
-        1) The first date isn't after the second date
-        2) They are not the same day
+    Ensures inputs are valid, and converts them to datetime.datetime objects if they are 
+    strings.
     """
+    start_is_valid_str = False
+    end_is_valid_str   = False
+
+    start_type = type(start_date)
+    end_type   = type(end_date)
+    
+    if start_type == str:
+        start_date = string_to_datetime(start_date)
+        start_is_valid_str = True
+    if end_type == str:
+        end_date = string_to_datetime(end_date)
+        end_is_valid_str = True
+    
+    if isinstance(start_type, datetime.datetime) or isinstance(start_type, datetime.date):
+        pass
+    elif start_is_valid_str == False:
+        raise TypeError(
+            "Start date must be a string, datetime.datetime or datetime.date object."
+        )
+    
+    if end_date == None:
+        # Start date is valid, no end date given.
+        return(start_date, end_date) 
+    elif isinstance(end_type, datetime.datetime) or isinstance(end_type, datetime.date):
+        pass
+    elif end_is_valid_str == False:
+        raise TypeError(
+            "When supplied, end date must be a string, datetime.datetime or datetime.date object."
+        )
+    
     if start_date > end_date:
         raise ValueError("Start date is after end date.")
     elif start_date == end_date:
         raise ValueError("Time interval must be one day or longer.")
+    else:
+        return(start_date, end_date)
 
 def day_with_suffix(day):
     """
@@ -35,48 +66,45 @@ def custom_strftime(format, date):
     day = day_with_suffix(date.day)
     return(date.strftime(format).replace('{S}', day))
 
-
-def friendlify(start_date, end_date):
+def friendlify(start_date, end_date=None):
     """
-    Returns interval of given dates in a human friendly format.
-
-    input: two dates either as strings in DD-MM-YYYY format, 
-           or as a datetime.datetime objects.
-    ouput: string
-    """
+    Returns interval of given dates in a human friendly string. 
+    If end_date not given, returns human friendly string of start_date.
     
-    if type(start_date) == str:
-        start_date = string_to_datetime(start_date)
-    if type(end_date) == str:
-        end_date = string_to_datetime(end_date)
+    Arguments can either be strings or datetime.datetime objects.
+    """
+    start, end = check_valid_input(start_date, end_date)
+    
+    if end==None:
+        return(custom_strftime('{S} %B, %Y', start))
 
-    check_interval(start_date, end_date)
-
-    if start_date.year == end_date.year:
-        if start_date.month == end_date.month:
+    if start.year == end.year:
+        if start.month == end.month:
             # Same month, same year
-            part1 = custom_strftime('{S}', start_date)
-            part2 = custom_strftime('{S} %B, %Y', end_date)
+            part1 = custom_strftime('{S}', start)
+            part2 = custom_strftime('{S} %B, %Y', end)
             return(part1 + " - " + part2)
         
         # Different month, same year
-        part1 = custom_strftime('{S} %B', start_date)
-        part2 = custom_strftime('{S} %B, %Y', end_date)
+        part1 = custom_strftime('{S} %B', start)
+        part2 = custom_strftime('{S} %B, %Y', end)
         return(part1 + " - " + part2)
     
     else:
         # Different month and year
-        part1  = custom_strftime('{S} %B, %Y', start_date)
-        part2  = custom_strftime('{S} %B, %Y', end_date)
+        part1  = custom_strftime('{S} %B, %Y', start)
+        part2  = custom_strftime('{S} %B, %Y', end)
         return(part1 + " - " + part2)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("start_date", 
-                        help="starting date for the time interval in form DD-MM-YYYY"
+                        help="starting date for the time interval in form DD-MM-YYYY",
                         )
     parser.add_argument("end_date", 
-                        help="ending date for the time interval in form DD-MM-YYYY"
+                        help="ending date for the time interval in form DD-MM-YYYY",
+                        nargs='?',
+                        default=None,
                         )
     args= parser.parse_args()
     print(friendlify(args.start_date, args.end_date))
